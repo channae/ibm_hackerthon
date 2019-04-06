c89dd2e68323dae29723f66c62e04d288bd814d9
Channa Edirisinghe <channa.lakruwan@gmail.com>
2019-04-06 14:57:33 +0800

    - singleton local database

--- app/src/main/java/com/android/squardb/ibmhackathon/data/source/local/BankAppDatabase.java
+++ app/src/main/java/com/android/squardb/ibmhackathon/data/source/local/BankAppDatabase.java
@@ -1,7 +1,9 @@
 package com.android.squardb.ibmhackathon.data.source.local;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import com.android.squardb.ibmhackathon.data.Account;
 import com.android.squardb.ibmhackathon.data.ExchangeRate;
@@ -11,6 +13,22 @@ import com.android.squardb.ibmhackathon.data.User;
 
 @Database(entities = {User.class, Transaction.class, ExchangeRate.class, Account.class}, version = 1, exportSchema = false)
 public abstract class BankAppDatabase extends RoomDatabase {
+
+    private static String DB_NAME = "ibm_hackathon_db";
+    private static volatile BankAppDatabase instance;
+
+    public static synchronized BankAppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+    public BankAppDatabase() {}
+
+    private static BankAppDatabase create(final Context context) {
+        return Room.databaseBuilder(context, BankAppDatabase.class, DB_NAME).allowMainThreadQueries().build();
+    }
 
     public abstract BankDao bankDao();
 
